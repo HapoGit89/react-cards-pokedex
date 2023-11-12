606cf60cea89843f4e18ce29e3bf2be9a27ef8a2
Hannes Porombka <hannes.porombka@gmail.com>
2023-11-13 00:52:58 +0100

    refactored PokeDex

--- src/hooks.js
+++ src/hooks.js
@@ -1,12 +1,25 @@
 import { useState } from "react";
+import axios from "axios";
+import uuid from "uuid";
 
 export function useFlip(facingUp) {
-  const [isFacingUp, setIsFacingUp] = useState(facingUp);
-  const flipCard = () => {
-    setIsFacingUp(isUp => !isUp);
-  };
-  
+    const [isFacingUp, setIsFacingUp] = useState(facingUp);
+    const flipCard = () => {
+        setIsFacingUp(isUp => !isUp);
+    };
 
-  return [isFacingUp, flipCard];
+
+    return [isFacingUp, flipCard];
+}
+
+
+export function useAxios(url) {
+    const [cards, setCards] = useState([]);
+    const addCard = async (name) => {
+        const response = await axios.get(`${url}/${name}`);
+        setCards(cards => [...cards, { ...response.data, id: uuid() }]);
+    };
+
+    return [cards, addCard];
 }
 
